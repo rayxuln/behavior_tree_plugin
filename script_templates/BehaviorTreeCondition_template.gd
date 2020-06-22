7dd9b5901e4cf6e87513027dbb6b146cefad760f
Raiix <imbosslx@sina.com>
2020-06-22 17:37:35 +0800

    Add readme

---
+++ script_templates/BehaviorTreeCondition_template.gd
@@ -0,0 +1,10 @@
+
+extends BehaviorTreeCondition
+
+# func <The node's name>():
+# 	pass
+
+
+# override
+func check(btn):
+	return true
---
+++ script_templates/BehaviorTreeNodeAction_template.gd
@@ -0,0 +1,18 @@
+
+extends BehaviorTreeNodeAction
+
+# func <The node's name>():
+# 	pass
+
+
+# override
+func enter():
+	pass
+
+# override
+func exit():
+	pass
+
+# override
+func execute():
+	return BTNResult.RUNNING
